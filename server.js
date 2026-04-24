9f40b022ca89f6a86898207512c37902dfe14c99
reytava96-design <reytava96@gmail.com>
2026-04-23 23:30:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let products = [];
+
+app.get("/", (req, res) => {
+    res.send("WorkPulse API rodando 🚀");
+});
+
+app.get("/products", (req, res) => {
+    res.json(products);
+});
+
+app.post("/products", (req, res) => {
+    products.push(req.body);
+    res.json({ message: "Produto adicionado", data: req.body });
+});
+
+// IMPORTANTE: porta do Azure
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", () => {
+    console.log("API rodando na porta " + port);
+});
