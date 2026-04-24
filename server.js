3039d05bbb73cc0b391b721c12485646fe2b2938
reytava96-design <reytava96@gmail.com>
2026-04-24 00:10:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,12 @@ const app = express();
 
 app.use(express.json());
 
-let products = [];
-
 app.get("/", (req, res) => {
     res.send("WorkPulse API rodando 🚀");
 });
 
 app.get("/products", (req, res) => {
-    res.json(products);
-});
-
-app.post("/products", (req, res) => {
-    products.push(req.body);
-    res.json({ message: "Produto adicionado", data: req.body });
+    res.json([]);
 });
 
 const port = process.env.PORT || 3000;
